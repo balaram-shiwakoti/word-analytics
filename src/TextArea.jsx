6e0233ba45cf117dafa7b3bb055c73b4f51c0659
Balaram Shiwakoti <prajinshiwakoti@gmail.com>
2024-05-08 13:30:18 +0545

    feat: added basic validation

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import Warning from "./Warning";
+
 export default function TextArea() {
-  return <textarea className="textarea">TextArea</textarea>;
+  const [text, setText] = useState("");
+  const [warningText, setWarningText] = useState("");
+
+  const handleChangeEvent = (e) => {
+    let newText = e.target.value;
+    //basic validation
+    if (newText.includes("<script>")) {
+      setWarningText("script tag");
+      newText = newText.replace("<script>", "");
+    } else if (newText.includes("@")) {
+      setWarningText("@ symbol");
+      newText = newText.replace("@", "");
+    } else {
+      setWarningText("");
+    }
+
+    setText(newText);
+  };
+  return (
+    <div className="textarea">
+      <textarea value={text} onChange={handleChangeEvent} />
+      {warningText ? <Warning warningText={warningText} /> : null}
+    </div>
+  );
 }
