59e7c706cb4733f956e98cf63c924f1b3c9911be
Balaram Shiwakoti <prajinshiwakoti@gmail.com>
2024-05-08 14:04:49 +0545

    feat: added logic for the count

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Stats from "./Stats";
 import TextArea from "./TextArea";
 
 export default function Container() {
+  const [text, setText] = useState("");
+
+  const stats = {
+    numberOfCharacters: text.length,
+    wordCount: text.split(/\s/).filter((word) => word !== "").length,
+    facebookCharacterCount: 2200 - text.length,
+    twitterCharacterCount: 280 - text.length,
+  };
   return (
     <main className="container">
-      <TextArea />
-      <Stats />
+      <TextArea text={text} setText={setText} />
+      <Stats stats={stats} />
     </main>
   );
 }
--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,10 +1,10 @@
-export default function Stats() {
+export default function Stats({ stats }) {
   return (
     <section className="stats">
-      <Stat count={0} label="words" />
-      <Stat count={0} label="characters" />
-      <Stat count={2200} label="facebook" />
-      <Stat count={280} label="x" />
+      <Stat count={stats.wordCount} label="words" />
+      <Stat count={stats.numberOfCharacters} label="characters" />
+      <Stat count={stats.facebookCharacterCount} label="facebook" />
+      <Stat count={stats.twitterCharacterCount} label="x" />
     </section>
   );
 }
@@ -12,7 +12,11 @@ export default function Stats() {
 function Stat({ count, label }) {
   return (
     <section className="stat">
-      <span className="stat__number">{count}</span>
+      <span
+        className={`stat__number ${count < 0 ? "stat__number--limit" : ""}`}
+      >
+        {count}
+      </span>
       <h2 className="second-heading">{label}</h2>
     </section>
   );
