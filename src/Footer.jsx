70ee51d2c19645e841ad5ea8d85206ac6895003c
Balaram Shiwakoti <prajinshiwakoti@gmail.com>
2024-05-08 12:34:27 +0545

    feat: made all skeleton component with css

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>&copy; Copyyright by Balaram. All rights reserved.</small>
+      <small>Last checked limits:10 Days ago</small>
+    </footer>
+  );
+}
